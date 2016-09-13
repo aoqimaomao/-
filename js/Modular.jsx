3d7c6dceb71804464ab973bc454910a94dac3724
aoqimaomao <18969905156@189.cn>
2016-09-13 09:58:47 +0800

    配置完善

--- js/Modular.jsx
+++ js/Modular.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 class Modular extends React.Component {
    render() {
       return (
-          <div className="draggable"  style={{display:"inline-block",padding:"1rem"}}>
-             <div id="modularImg" className="part" style={{ width: '142px', height: '110px', background: '#aaa'}}>
-                <img src={this.props.url} style={{ width: '142px', height: '110px'}}/>
+          <div className="draggable"  style={{display:"inline-block",padding:"1rem",position:"relative"}}>
+             <div id="modularImg" className="part" style={{width: '142px', height: '110px', background: '#aaa'}}>
+                <img src={this.props.url} style={{ width: '100%', height: '100%'}}/>
              </div>
-             <div>
+             <div className="heading">
                 <h3 style={{fontSize:"1.4rem",fontWeight:"600",textAlign:"center",lineHeight:"3.2rem"}}>{this.props.title}</h3>
                  <span className="edit">
                      <i className="icon fa fa-pencil-square-o" style={{marginLeft:"0"}}></i>
