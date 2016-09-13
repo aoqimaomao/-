60a49b458a5eae16eb2c97d28ebf095fc4dbfe94
aoqimaomao <18969905156@189.cn>
2016-09-13 10:05:32 +0800

    位置调整

--- js/Modular.jsx
+++ js/Modular.jsx
@@ -6,13 +6,14 @@ class Modular extends React.Component {
           <div className="draggable"  style={{display:"inline-block",padding:"1rem",position:"relative"}}>
              <div id="modularImg" className="part" style={{width: '142px', height: '110px', background: '#aaa'}}>
                 <img src={this.props.url} style={{ width: '100%', height: '100%'}}/>
-             </div>
-             <div className="heading">
-                <h3 style={{fontSize:"1.4rem",fontWeight:"600",textAlign:"center",lineHeight:"3.2rem"}}>{this.props.title}</h3>
                  <span className="edit">
                      <i className="icon fa fa-pencil-square-o" style={{marginLeft:"0"}}></i>
                      <i className="icon fa fa-trash-o"></i>
                  </span>
+             </div>
+             <div className="heading">
+                <h3 style={{fontSize:"1.4rem",fontWeight:"600",textAlign:"center",lineHeight:"3.2rem"}}>{this.props.title}</h3>
+
 
              </div>
           </div>
